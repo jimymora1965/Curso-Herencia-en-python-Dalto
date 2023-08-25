6b18e68e5dcf3e89bdaa3f813b24d88573a1c490
jimy <jimymora@hotmail.com>
2023-08-25 18:31:39 -0500

    clinica 2

--- Herencia multiple/herencia_multiple_clinica.py
+++ Herencia multiple/herencia_multiple_clinica.py
@@ -2,32 +2,31 @@ class Clinica:
     def __init__(self, nombre, apellido, especialidad):
       self.n = nombre 
       self.a = apellido 
-      self.e = especialidad
-    
+      self.esp = especialidad
+      
     
 class Sede:
     def __init__(self, departamento, ciudad):
        self.d = departamento
        self.e = ciudad
-    
 class Universidad:
     def __init__(self,javeriana, nacional, antioquia):
        self.j = javeriana 
        self.nal = nacional 
        self.ant = antioquia 
-    
 class Galeno(Clinica,Sede,Universidad):
     def __init__(self,nombre,apellido,especialidad,departamento,ciudad,javeriana,nacional,antioquia, salario):
        Clinica.__init__(self,nombre,apellido,especialidad)
        Sede.__init__(self,departamento,ciudad)
        Universidad.__init__(self,javeriana,nacional,antioquia)
-       self.sal = salario 
-       
+       self.sal = salario
+   
+  
 ginecologo = Galeno("Felipe","Petro","Ginecologo","Antioquia","Medellin","Javeriana","Nacional","Antioquia",10000000)
-print(ginecologo.a,ginecologo.n,ginecologo.e)
 
 
-       
-    
- 
-    
+print(ginecologo.n,ginecologo.a,ginecologo.j,ginecologo.esp)
+
+
+
+
---
+++ Herencia multiple/herencia_multiple_clinica2.py
@@ -0,0 +1,35 @@
+class Clinica:
+    def __init__(self,nombre,apellido,especialidad):
+        self.n = nombre
+        self.a = apellido
+        self.esp = especialidad
+        
+class TipoVinculacion:
+    def __init__(self, vinculado, prestacion_de_servicios):
+        self.vin = vinculado
+        self.ps = prestacion_de_servicios 
+        
+class Sede:
+    def __init__(self, departamento, ciudad):
+        self.dpto = departamento
+        self.ciu = ciudad
+        
+class Especialidad:
+    def __init__(self, anestesiologo,cirujano, ortopedia):
+        self.anes = anestesiologo
+        self.cx = cirujano
+        self.ort = ortopedia
+        
+class Galeno(Clinica,TipoVinculacion,Sede,Especialidad):
+    def __init__(self,nombre, apellido,especialidad,vinculado, prestacion_de_servicios,departamento,ciudad,anestesiologo,cirujano,ortopedia,salario):
+        Clinica.__init__(self,nombre,apellido,especialidad)
+        TipoVinculacion.__init__(self,vinculado, prestacion_de_servicios)
+        Sede.__init__(self,departamento,ciudad)
+        Especialidad.__init__(self,anestesiologo,cirujano,ortopedia)
+        self.sal = salario
+        
+
+especialista = Galeno("Jimy","Mora","Anestesiologo","Vinculado","Prestador_Servicios","Antioquia","Medellin","Anestesiologo","cirujano","ortopedista",10000)
+print(especialista.n,especialista.esp)
+        
+        
